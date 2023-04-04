650989071593458aa144685afc741ac05338c4b5
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-04-04 11:47:20 -0400

    update test

--- test_python_R.R
+++ test_python_R.R
@@ -1,4 +1,16 @@
+
 #test run python
 install.packages('reticulate')
 
+
+# install python packages
+reticulate::py_install('torch')
+
+# run python script
 reticulate::source_python('c1.py')
+
+# find forecast_fil to submit to challenge
+team_name <- 'VT_aquaers'
+forecast_date <-  Sys.Date()
+
+forecast_file <- paste0(paste('aqautics', forecast_date, team_name), '.csv.gz')
