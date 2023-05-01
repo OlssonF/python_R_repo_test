4b3f07755ba0829e8dbf0ba487a821d89d48a29f
Freya Olsson <freyao@vt.edu>
2023-05-01 22:25:53 +0000

    loop

--- test_python_R.R
+++ test_python_R.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(lubridate)
+
 
 # Step 1: install additional R packages needed
 remotes::install_github('eco4cast/neon4cast')
@@ -8,22 +11,39 @@ remotes::install_github('eco4cast/neon4cast')
 # Step 2: install python packages using reticulate
 to_install <- scan('requirements.txt', character(), quote = "")
 reticulate::py_install(packages = to_install)
+message('package install complete')
 
+dates <- seq.Date(as_date('2023-03-01'),
+                  Sys.Date(),
+                  'day') 
 
+for (i in 1:length(dates)) {
+  
+  forecast_date <-  dates[i]
+  
+  message('starting python script')
+  # Step 3: run python script - include the import etc.
+  system(paste0('python forecast_script.py ', forecast_date))
+  # this should write the csv.gz file at the end
+  
+  
+  
+  
+  # Step 4: find forecast_file to submit to challenge
+  team_name <- 'VT_Aquaers'
+  # forecast_date <-  Sys.Date()
+  theme <- 'aquatics'
+  
+  # set to standard
+  forecast <- readr::read_csv('lake.csv.gz') |>
+    dplyr::mutate(reference_datetime = min(datetime) - lubridate::days(1)) |> 
+    dplyr::rename(parameter = parameters)
+  
+  
+  forecast_file <- paste0(paste(theme, forecast_date, team_name, sep = '-'), '.csv.gz')
+  
+  readr::write_csv(forecast, forecast_file)
+  
+  neon4cast::submit(forecast_file)
+}
 
-
-# Step 3: run python script - include the import etc.
-reticulate::source_python('forecast_script.py') 
-# this should write the csv.gz file at the end
-
-
-
-
-# Step 4: find forecast_file to submit to challenge
-team_name <- 'your_team_name'
-forecast_date <-  Sys.Date()
-theme <- 'aquatics'
-
-forecast_file <- paste0(paste(theme, forecast_date, team_name), '.csv.gz')
-
-neon4cast::submit(forecast_file)
